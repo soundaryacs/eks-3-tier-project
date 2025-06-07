9cd9e3793af7287d5e918c88b52750296be9de54
Soundarya CS <soundaryacs92@gmail.com>
2025-06-07 15:06:24 +0530

    r53

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.84.145.194:84";
- const API_BASE_URL = "http://dev.637423460436.realhandsonlabs.net";
+ const API_BASE_URL = "http://dev.590184008189.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
